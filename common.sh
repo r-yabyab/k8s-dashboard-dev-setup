7a01d01da6a3d1d1007ae2e85fb2d31cd1d6f2c9
Scruffy pc <cayabyabrr@yahoo.com>
2024-05-13 20:13:21 -0700

    setup

---
+++ common.sh
@@ -0,0 +1,50 @@
+sudo apt-get update
+
+#Install go
+sudo su
+sudo wget https://go.dev/dl/go1.22.3.linux-amd64.tar.gz
+rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.3.linux-amd64.tar.gz
+
+sudo sh -c 'echo "export PATH=\$PATH:/usr/local/go/bin" >> /etc/profile'
+source /etc/profile
+exit
+
+# put in end of line .bashrc
+# export GOPATH=/home/ubuntu/go
+# export PATH=$PATH:$GOPATH/bin
+# NOT THIS ONE export PATH=$PATH:/usr/local/go/bin
+
+#install nodejs and npm
+sudo su
+curl -fsSL https://deb.nodesource.com/setup_22.x | bash - &&\
+apt-get install -y nodejs
+exit
+
+#install yarn
+sudo npm install --global yarn
+
+#install docker
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo usermod -aG docker "$whoami"
+
+sudo git clone https://github.com/kubernetes/dashboard.git
+
+sudo su
+cd modules/web && yarn
+
+exit
+
