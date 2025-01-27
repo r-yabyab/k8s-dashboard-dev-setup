a2887e7ed72839742aea0ad9b2f0eb8e68d271b7
Scruffy pc <cayabyabrr@yahoo.com>
2025-01-27 14:28:49 -0800

    Create kind cluster

--- nephio/modules.sh
+++ nephio/modules.sh
@@ -12,7 +12,7 @@ go install -v github.com/GoogleContainerTools/kpt@main
 kpt version
 
 
-# kind create cluster -n mgmt
+kind create cluster -n mgmt
 
 # kubectl download
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
