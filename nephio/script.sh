7f4b6540aaa41df08be44a163ec9216826920433
Scruffy pc <cayabyabrr@yahoo.com>
2025-02-14 13:54:22 -0800

    install ca-certificates

--- nephio/script.sh
+++ nephio/script.sh
@@ -23,7 +23,7 @@ source $HOME/.bashrc
 
 # Install Docker
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
