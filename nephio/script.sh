d824f8f62eb4d395a9354002dfd30044f675f7c8
Scruffy pc <cayabyabrr@yahoo.com>
2025-05-20 11:35:38 -0500

    go version

--- nephio/script.sh
+++ nephio/script.sh
@@ -17,7 +17,7 @@ VERSION=1.24.3
 # Install Go
 # wget https://go.dev/dl/go1.24.3.linux-amd64.tar.gz
 wget https://go.dev/dl/go${VERSION}.linux-amd64.tar.gz
-sudo sh -c 'rm -rf /usr/local/go && tar -C /usr/local -xzf go${VERSION}.linux-amd64.tar.gz'
+sudo sh -c "rm -rf /usr/local/go && tar -C /usr/local -xzf go${VERSION}.linux-amd64.tar.gz"
 sudo sh -c 'echo "export PATH=\$PATH:/usr/local/go/bin" > /etc/profile.d/go.sh'
 source /etc/profile.d/go.sh
 
