63c1ae9bcb14bf6239bddc75fb916518d1cd2a1c
Scruffy pc <cayabyabrr@yahoo.com>
2025-05-20 11:51:50 -0500

    porch local setup

--- nephio/porch local/script.sh
+++ nephio/porch local/script.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-#make
+# install make
+sudo apt-get install make
+
+# clone porch
+git clone https://github.com/nephio-project/porch.git
+
+#download deps
+cd porch
+make tidy
+
+make run
+
+#then set up porchctl
+cd porch/cmd/porchctl
+go build
+
+#add binary to PATH
+# mv porchctl /usr/local/bin
