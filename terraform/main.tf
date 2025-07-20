ab192ff6a41a226170b37b5fff6ba7fa91c27f20
Scruffy pc <cayabyabrr@yahoo.com>
2025-07-20 16:22:59 -0500

    cuda driver instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,19 +8,29 @@ resource "aws_instance" "my_instance_worker" {
   # 22.04
   # ami           = "ami-0606dd43116f5ed57"
   
-  instance_type = "c5ad.2xlarge"
+  # instance_type = "c5ad.2xlarge"
   # 8vCPU, 16GB RAM
   # $0.3400 per hour
+
   # instance_type = "t3.large"
   # 2vCPU, 8GB RAM
   # $0.0836 per hour
+
+  #this one doesn't have nvidia gpu
+  # instance_type = "t2.2xlarge"
+  # 8vCPU, 32GB RAM
+  # $0.3852 per hour
+
+  instance_type ="g4dn.xlarge"
+  # 4vCPU, 16GB RAM, 1 NVIDIA T4 GPU
+  # $0.526 per hour
   tags = {
     "Name" = "k8s-dashboard-dev-master"
   }
   key_name        = "kube-server"
   security_groups = ["k8s-dashboard-dev"]
   root_block_device {
-    volume_size = 30
+    volume_size = 50
 # 30 for k8s dashboard, 50 nephio
   }
 }
