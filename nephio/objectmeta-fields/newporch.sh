4b28fc988604d72cb112afeb3328388dc23c2dfc
Scruffy pc <cayabyabrr@yahoo.com>
2025-09-15 21:12:18 -0500

    notes

--- nephio/objectmeta-fields/newporch.sh
+++ nephio/objectmeta-fields/newporch.sh
@@ -2,12 +2,20 @@
 # https://docs.nephio.org/docs/porch/contributors-guide/environment-setup/ # local
 # https://docs.nephio.org/docs/porch/user-guides/install-porch/ # for kpt, not local
 
+# about 8gb vol to set up kind, porch, kpt-backstage dev env
+
 #create porch-test kind cluster
 kind create cluster -n porch-test
 
 # install kpt
 go install github.com/kptdev/kpt@main
 kpt version
+
+# to try this out, crashes if porch is set up already
+cd porch/cmd/porchctl
+go build
+#add binary to PATH
+sudo mv porchctl /usr/local/bin
 
 # see list of make builds
 make help 
@@ -27,3 +35,10 @@ cd kpt-backstage-plugins/hack
 
 # skips api-server, for dev. Take slike 30 seconds opposed to 5 minutes with apiserver
 make run-in-kind-no-server
+
+
+# porch\pkg\registry\porch\packagerevisionresources.go
+
+
+# for setting up porchctl
+# need to make token for fresh github repo
